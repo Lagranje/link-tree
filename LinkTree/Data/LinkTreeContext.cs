a4b7c629dd8035b3622f97381eaca438a46121e8
lagrange <contantinator@gmail.com>
2020-08-12 14:16:10 +0300

    added UserModel

---
+++ LinkTree/Data/LinkTreeContext.cs
@@ -0,0 +1,27 @@
+﻿using LinkTree.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkTree.Data
+{
+    public class LinkTreeContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+               .SetBasePath(Environment.CurrentDirectory)
+               .AddJsonFile("appsettings.json")
+               .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.Use(connectionString);
+        }
+    }
+}
---
+++ LinkTree/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkTree.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public string Nickname { get; set; }
+
+
+        public string AvatarUrl { get; set; }
+
+        public DateTime RegDate { get; set; }
+    }
+}
